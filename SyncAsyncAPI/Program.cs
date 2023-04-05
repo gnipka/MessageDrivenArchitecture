47003f1f21b2d47fea3adca258cca83f85665a3c
gnipka <gnipka29@gmail.com>
2023-04-05 13:18:42 +0300

    Sample

--- SyncAsyncAPI/Program.cs
+++ SyncAsyncAPI/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace SyncAsyncAPI
-{    
+﻿using System.Diagnostics;
+
+namespace SyncAsyncAPI
+{
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            var rest = new Restaurant();
+            while (true)
+            {
+                Console.WriteLine("Привет! Желаете забронировать столик?\n1 - мы уведомим Вас по смс (асинхронно)" +
+                    "\n2 - подождите на линии, мы Вас оповестим (синхронно)");
+                if (!int.TryParse(Console.ReadLine(), out var choice) && choice is not (1 or 2))
+                {
+                    Console.WriteLine("Введите пожалуйста 1 или 2");
+                    continue;
+                }
+
+                var stopWatch = new Stopwatch();
+                stopWatch.Start();
+                if (choice == 1) // смс
+                {
+                    rest.BookFreeTableAsync(1);
+                }
+                else // звонок
+                {
+                    rest.BookFreeTable(1);
+                }
+
+                Console.WriteLine("Спасибо за Ваше обращение!");
+                stopWatch.Stop();
+                var ts = stopWatch.Elapsed;
+                Console.WriteLine($"{ts.Seconds:00}:{ts.Milliseconds:00}"); // потраченное время
+            }
         }
     }
 }
---
+++ SyncAsyncAPI/Restaurant.cs
@@ -0,0 +1,47 @@
+﻿namespace SyncAsyncAPI
+{
+    public class Restaurant
+    {
+        private readonly List<Table> _tables = new();
+
+        public Restaurant()
+        {
+            for (ushort i = 1; i <= 10; i++)
+            {
+                _tables.Add(new Table(i));
+            }
+        }
+
+        public void BookFreeTable(int countOfPersons)
+        {
+            Console.WriteLine("Добрый день! Подождите секунду я подберу столик и подтвержу вашу бронь, оставайтесь на линии");
+
+            var table = _tables.FirstOrDefault(t => t.SeatsCount > countOfPersons && t.State == State.Free);
+
+            Thread.Sleep(1000 * 5); // в течение 5 секунд происходит поиск стола
+            table?.SetState(State.Booked);
+
+            Console.WriteLine(table is null
+                ? "К сожалению, сейчас все столики заняты"
+                : $"Готово! Ваш столик номер {table.Id}");
+        }
+
+        public void BookFreeTableAsync(int countOfPersons)
+        {
+            Console.WriteLine("Добрый день! Подождите секунду я подберу столик и подтвержу вашу бронь, Вам придет уведомление");
+
+            Task.Run(async () =>
+            {
+                var table = _tables.FirstOrDefault(t => t.SeatsCount > countOfPersons && t.State == State.Free);
+
+                await Task.Delay(1000 * 5);
+                table?.SetState(State.Booked);
+
+
+                Console.WriteLine(table is null
+                    ? "УВЕДОМЛЕНИЕ: К сожалению, сейчас все столики заняты"
+                    : $" УВЕДОМЛЕНИЕ: Готово! Ваш столик номер {table.Id}");
+            });
+        }
+    }
+}
